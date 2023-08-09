7116f868ba6bf312b36baa79bbe296050791f5e5
JingkaiSHI <shi_jing_kai@outlook.com>
2023-08-09 11:35:43 -0700

    Tile entity initialization, creation of template

--- README.txt
+++ README.txt
@@ -14,3 +14,5 @@ Update Log:
 2023/8/3: Set up deferred register for blocks, items, tile entity and creative tabs.
 2023/8/4: Add hologram base into the game, encountered bugs in the process.
 2023/8/7: added the base into the game, fixed previous mentioned bug, found more problems
+2023/8/8: fixed texture of hologram base problem, started to work on core of the mod: introduce GPT API inside
+2023/8/9: introduced tile entity associated with the block, will attend to cosmetic detail of the hologram later with a refactor
--- src/main/java/net/jingkaishi/mineassistantmod/common/tileentities/custom/HologramTileEntity.java
+++ src/main/java/net/jingkaishi/mineassistantmod/common/tileentities/custom/HologramTileEntity.java
@@ -1,22 +1,62 @@
 package net.jingkaishi.mineassistantmod.common.tileentities.custom;
 
 import net.jingkaishi.mineassistantmod.common.tileentities.ModTileEntities;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.jingkaishi.mineassistantmod.MineAssistantMod;
 import net.jingkaishi.mineassistantmod.common.blocks.ModBlocks;
 
-public class HologramTileEntity extends TileEntity implements ITickableTileEntity {
+import javax.annotation.Nonnull;
+
+
+public class HologramTileEntity extends TileEntity {
 
     public HologramTileEntity() {
         super(ModTileEntities.HOLOGRAM.get());
     }
 
-    @Override
-    public void tick() {
-        // Here goes the animation logic
+    private ItemStackHandler createHandler(){
+        return new ItemStackHandler(1){
+
+
+            // do the save
+            @Override
+            protected void onContentsChanged(int slot) {
+                setChanged();
+            }
+
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                switch (slot){
+                    case 0: return stack.getItem() == Items.WRITABLE_BOOK;
+                    default:
+                        return false;
+                }
+            }
+
+            @Override
+            public int getSlotLimit(int slot) {
+                return 1;
+            }
+
+            @Nonnull
+            @Override
+            public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+                if(!isItemValid(slot, stack)){
+                    return stack;
+                }
+                return super.insertItem(slot, stack, simulate);
+            }
+        };
     }
+
+
 }
